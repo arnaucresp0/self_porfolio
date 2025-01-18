a9f26b0935666d1ee7ff8e370c01bb37186be0c5
arnaucresp0 <acs2000f20@gmail.com>
2025-01-18 01:39:05 +0100

    add animated cursor

--- script.js
+++ script.js
@@ -16,5 +16,22 @@ document.addEventListener("DOMContentLoaded", () => {
         title.classList.toggle("active");
       });
     });
+
+    // Create the custom cursor
+    document.addEventListener('mousemove', (e) => {
+        const trail = document.createElement('div');
+        trail.classList.add('trail');
+        document.body.appendChild(trail);
+      
+        trail.style.left = `${e.pageX}px`;
+        trail.style.top = `${e.pageY}px`;
+      
+        // Remove the trail after the animation ends
+        setTimeout(() => {
+          trail.remove();
+        }, 600); // Match animation duration
+      });
+      
+
   });
   
